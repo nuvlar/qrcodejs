40c1a66e3480749bbea7a8c85e663103f5ea037f
Miguel <miguel@nuvlar.com>
2025-06-04 02:07:11 -0600

    Removed android check

--- qrcode.js
+++ qrcode.js
@@ -157,19 +157,8 @@ var QRCode;
 	
 	// android 2.x doesn't support Data-URI spec
 	function _getAndroid() {
-		var android = false;
-		var sAgent = navigator.userAgent;
-		
-		if (/android/i.test(sAgent)) { // android
-			android = true;
-			var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
-			
-			if (aMat && aMat[1]) {
-				android = parseFloat(aMat[1]);
-			}
-		}
-		
-		return android;
+
+		return false;
 	}
 	
 	var svgDrawer = (function() {
